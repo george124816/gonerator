4dcaed4e7d0a8dcc65f0f2bf0b1a357b3039f5f0
George Rodrigues <georgerodrigues@live.com.au>
2025-04-03 17:04:55 -0300

    feat: add cpf and cnpj function

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	basic_commands := `basic commands:
   uuid          generate a valid uuid
+  cpf           generate a valid cpf
+  cnpj          generate a valid cnpj
 
 usage:
   generator [command]
@@ -21,6 +23,10 @@ usage:
 		switch os.Args[1] {
 		case "uuid":
 			fmt.Println(generate_uuid())
+		case "cpf":
+			fmt.Println(generateCPF())
+		case "cnpj":
+			fmt.Println(generateCNPJ())
 		default:
 			fmt.Println(basic_commands)
 
@@ -68,4 +74,44 @@ func get_random_char() (char string) {
 	default:
 		return strconv.Itoa(i)
 	}
+}
+
+func generateCPF() string {
+	var cpf [11]int
+	for i := 0; i < 9; i++ {
+		cpf[i] = rand.Intn(10)
+	}
+
+	// Calcula os dois dígitos verificadores
+	cpf[9] = calculateDigit(cpf[:9], 10)
+	cpf[10] = calculateDigit(cpf[:10], 11)
+
+	return fmt.Sprintf("%d%d%d%d%d%d%d%d%d%d%d", cpf[0], cpf[1], cpf[2], cpf[3], cpf[4], cpf[5], cpf[6], cpf[7], cpf[8], cpf[9], cpf[10])
+}
+
+func generateCNPJ() string {
+	var cnpj [14]int
+	for i := 0; i < 12; i++ {
+		cnpj[i] = rand.Intn(10)
+	}
+	cnpj[12] = calculateDigit(cnpj[:12], 5)
+	cnpj[13] = calculateDigit(cnpj[:13], 6)
+
+	return fmt.Sprintf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d", cnpj[0], cnpj[1], cnpj[2], cnpj[3], cnpj[4], cnpj[5], cnpj[6], cnpj[7], cnpj[8], cnpj[9], cnpj[10], cnpj[11], cnpj[12], cnpj[13])
+}
+
+func calculateDigit(base []int, weightStart int) int {
+	sum, weight := 0, weightStart
+	for _, num := range base {
+		sum += num * weight
+		weight--
+		if weight < 2 {
+			weight = 9
+		}
+	}
+	digit := 11 - (sum % 11)
+	if digit >= 10 {
+		digit = 0
+	}
+	return digit
 }
